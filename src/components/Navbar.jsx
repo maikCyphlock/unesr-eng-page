b823fa0f43e10380fcfefcce0218d596eebec849
maikCyphlock <maikolaguilar656@gmail.com>
2022-10-28 20:19:42 -0400

    modify of style of navbar and removing button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import {Link} from 'react-router-dom';
+import React from 'react'
+import { Link } from 'react-router-dom'
 
-function Button({text, bg, padding}) {
+function Button ({ text, bg, padding }) {
   return (
     <div>
       <button
@@ -12,30 +12,27 @@ function Button({text, bg, padding}) {
         <span>{text}</span>
       </button>
     </div>
-  );
+  )
 }
 
-
-function Navbar() {
+function Navbar () {
   return (
-    <div className="sticky left-0 right-0 top-0 h-16 shadow-md bg-gray-900">
-      <nav className="flex items-center container mx-auto h-full justify-between">
-        <h1 className="font-semibold uppercase text-lg text-gray-200">
+    <div className='sticky left-0 right-0 top-0 h-16 shadow-md bg-gray-800'>
+      <nav className='flex items-center container mx-auto h-full sm:justify-between justify-center'>
+        <h1 className='font-semibold uppercase text-lg text-gray-200 hidden sm:contents'>
           Unesr
         </h1>
         <div>
-          <ul className="flex items-center space-x-10 text-sm">
-            <li><Link to="/" className="text-gray-400 hover:text-gray-100">Home</Link></li>
-            <li><Link to="/about" className="text-gray-400 hover:text-gray-100">About Us</Link></li>
-            <li><Link to="/docs" className="text-gray-400 hover:text-gray-100">Docs</Link></li>
+          <ul className='flex items-center space-x-10 text-sm'>
+            <li><Link to='/' className='text-gray-400 hover:text-gray-100'>Home</Link></li>
+            <li><Link to='/about' className='text-gray-400 hover:text-gray-100'>About Us</Link></li>
+            <li><Link to='/docs' className='text-gray-400 hover:text-gray-100'>Docs</Link></li>
           </ul>
         </div>
-        <div>
-          <Button text="Login" bg="bg-blue-400"/>
-        </div>
+        <div />
       </nav>
     </div>
-  );
+  )
 }
 
-export default Navbar;
+export default Navbar
