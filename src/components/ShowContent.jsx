1e94a0db78802dbd4d7e9fc33c8c3e5ad719f961
maikCyphlock <maikolaguilar656@gmail.com>
2022-10-28 20:20:06 -0400

    standard js fix

--- src/components/ShowContent.jsx
+++ src/components/ShowContent.jsx
@@ -1,39 +1,47 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react'
 
-import { useParams } from "react-router-dom";
+import { useParams } from 'react-router-dom'
 
 const Error = () => {
-  return(
-    <h1 className="mx-auto text-1xl md:text-3xl font-bold">
+  return (
+    <h1 className='mx-auto text-1xl md:text-3xl font-bold'>
       Ups.. parece que haz intentado acceder a un sitio que no existe
     </h1>
   )
 }
 
-export default function ShowContent() {
-  let { id } = useParams();
-  const [Content, setContent] = useState({});
+export default function ShowContent () {
+  const { id } = useParams()
+  const [Content, setContent] = useState({})
   const [error, SetError] = useState()
 
-  async function ReadContentFromMd() {
-    const module = await import(`../content/${id}.md`);
-    return module;
+  async function ReadContentFromMd () {
+    const module = await import(`../content/${id}.md`)
+    return module
   }
 
   useEffect(() => {
-    const result = ReadContentFromMd().then(Markdown => setContent({ attributes: Markdown.attributes, html: Markdown.html })).catch(errByReadingMd => SetError(errByReadingMd));
+    const result = ReadContentFromMd()
+      .then((Markdown) =>
+        setContent({ attributes: Markdown.attributes, html: Markdown.html })
+      )
+      .catch((errByReadingMd) => SetError(errByReadingMd))
 
-    return () => {
-      result;
-    };
-  }, []);
+    return () => result
+  }, [])
 
   return (
-    <div className="h-min-screen  ">
-      {
-        error ? <Error/> : <article className="px-1 mx-auto prose prose-sm md:prose-lg  lg:prose-xl mt-20" dangerouslySetInnerHTML={{ __html: Content?.html }}></article>
-      }
-      
+    <div className='h-min-screen  '>
+      {error
+        ? (
+          <Error />
+          )
+        : (
+          <article
+            className='px-1 mx-auto prose prose-sm md:prose-lg  lg:prose-xl mt-20'
+            dangerouslySetInnerHTML={{ __html: Content?.html }}
+          />
+          )}
     </div>
-  );
+  )
 }
--- src/pages/course/index.jsx
+++ src/pages/course/index.jsx
@@ -1,13 +1,12 @@
-import React, { Suspense, lazy } from "react";
-import Loading from "../../components/Loading";
+import React, { Suspense, lazy } from 'react'
+import Loading from '../../components/Loading'
 
-const ShowContent = lazy(()=> import("../../components/ShowContent"))   
-export default function Index() {
-    return (
-      <Suspense fallback={<Loading/>}>
-        
-       <ShowContent/>
-      </Suspense>
-    );
-  }
-  
+const ShowContent = lazy(() => import('../../components/ShowContent'))
+export default function Index () {
+  return (
+    <Suspense fallback={<Loading />}>
+
+      <ShowContent />
+    </Suspense>
+  )
+}
